8e3f10dc6b19500165a98a29f788d23a0700116c
Abhijit Rath <153931918+rathAbhijit@users.noreply.github.com>
2026-04-14 21:02:15 +0530

    Create Message model file defining the Message interface

---
+++ server/src/models/Message.ts
@@ -0,0 +1,9 @@
+export interface Message {
+    _id: string;
+    projectId: string;
+    userId: string;
+    username: string;
+    content: string;
+    timestamp: string;
+    updatedAt: string;
+}
